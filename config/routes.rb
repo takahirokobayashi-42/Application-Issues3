c5cd4edf4e318dfbf6b50151c55483f4c888f1b7
takahirokobayashi-42 <t.koban5820@gmail.com>
2020-12-02 17:46:26 +0000

    finish

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  devise_for :users
+  root to: 'homes#top'
+  get 'home/about' => 'homes#about', as: 'about'
+  resources :books, only: [:new, :create, :index, :show, :destroy, :edit, :update] do
+    resources :book_comments, only: [:create, :destroy]
+  resource :favorites, only: [:create, :destroy]
+  end
+  resources :users, only: [:show, :edit, :update, :index]
+end
+
